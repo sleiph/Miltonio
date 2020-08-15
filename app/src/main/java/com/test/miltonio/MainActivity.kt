60e0dea6dd4601ba05590c53c31703b04fbf401e
Ricardo Gomes <sleiph@gmail.com>
2020-08-14 22:05:09 -0300

    tentando implantar um banco de dados

--- app/src/main/java/com/test/miltonio/MainActivity.kt
+++ app/src/main/java/com/test/miltonio/MainActivity.kt
@@ -1,14 +1,85 @@
 package com.test.miltonio
 
 import android.app.Application
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
-import java.io.BufferedReader
-import java.io.FileNotFoundException
-import java.io.InputStreamReader
+import androidx.room.Room
+import androidx.room.*
+import androidx.sqlite.db.SupportSQLiteDatabase
+import java.util.concurrent.Executors
+
+//data class Categorias(val catg: String, var pnts: Int)
+@Entity(tableName = "tabela_categorias")
+class Categorias(
+    @PrimaryKey val cid: Int,
+    @ColumnInfo(name = "categoria") val categoria: String,
+    @ColumnInfo(name = "pontos") val pontos: Int
+)
+
+@Dao
+interface CategoriasDao {
+    @Query("SELECT * from tabela_categorias")
+    fun getAll(): List<Categorias>
+
+    @Query("SELECT * FROM tabela_categorias WHERE cid IN (:catgIds)")
+    fun loadAllByIds(catgIds: IntArray): List<Categorias>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(categoria: List<Categorias>)
+
+    @Update(entity = Categorias::class)
+    suspend fun updateCategory(categoria: Categorias)
+
+    @Delete
+    fun delete(user: Categorias)
+}
+
+@Database(entities = arrayOf(Categorias::class), version = 1, exportSchema = false)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun CategoriasDao(): CategoriasDao
+
+    companion object {
+        @Volatile private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context): AppDatabase {
+            return Room.databaseBuilder(context, AppDatabase::class.java, "tabela_categorias")
+                .addCallback(object : RoomDatabase.Callback() {
+                    override fun onCreate(db: SupportSQLiteDatabase) {
+                        super.onCreate(db)
+                        //pre-populate data
+                        Executors.newSingleThreadExecutor().execute {
+                            instance?.CategoriasDao()?.insert(DataGenerator.getCatgs())
+                        }
+                    }
+                })
+                .build()
+        }
+    }
+}
+
+class DataGenerator {
+
+    companion object {
+        fun getCatgs(): List<Categorias>{
+            return listOf(
+                Categorias(0, "Adm", 0),
+                Categorias(1, "Alg", 0),
+                Categorias(2, "Aoc", 0)
+            )
+        }
+    }
+
+}
 
 class MyApplication: Application() {
     companion object {
@@ -36,7 +107,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        fun readFromFile(fileName: String): String {
+        /*fun readFromFile(fileName: String): String {
 
             var ret = ""
 
@@ -65,7 +136,20 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        val dados = readFromFile( "dados.txt").split(",")
+        val dados = readFromFile( "dados.txt").split(",")*/
+
+        val db = Room.databaseBuilder(
+            applicationContext, AppDatabase::class.java,"tabela_categorias"
+        )
+            .fallbackToDestructiveMigration()
+            .fallbackToDestructiveMigrationOnDowngrade()
+            .fallbackToDestructiveMigrationFrom(16, 17, 18, 19)
+            .build()
+
+        db.query("SELECT * FROM tabela_categorias", null)
+        println("db")
+        println(db.toString())
+        println(db.query("SELECT * FROM tabela_categorias", null))
 
         val textos = arrayOf(
             findViewById<TextView>(R.id.txt_progresso_adm) as TextView,
@@ -86,7 +170,8 @@ class MainActivity : AppCompatActivity() {
         val cardPrg = findViewById<CardView>(R.id.CardViewPrg)
 
         for (i in 0..6)
-            textos[i].setText(getString(R.string.resultado_pontos, dados.get(i)))
+            //textos[i].setText(getString(R.string.resultado_pontos, dados.get(i)))
+            textos[i].setText(getString(R.string.resultado_pontos, "0"))
 
         cardAdm.setOnClickListener {
             loadRespostas(0)
