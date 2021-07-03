815f5aa5986e073ff180a27e50d4092e231e227a
Ricardo Gomes <sleiph@gmail.com>
2021-07-02 22:33:05 -0300

    simplificando e comentando o codigo

--- app/src/main/java/com/test/miltonio/ui/TelaPerguntas.kt
+++ app/src/main/java/com/test/miltonio/ui/TelaPerguntas.kt
@@ -28,36 +28,46 @@ class TelaPerguntas : AppCompatActivity() {
             startActivity(intent)
         }
 
-        // pega a materia escolhida do intent da tela anterior
+        // pegando o id do intent da tela anterior
         val resulIntent = intent
         val id = resulIntent.getIntExtra("id", 0)
-        val materia = MyApplication.materiasdatabase?.MateriaDao()?.loadById(id)
+        val materia = MyApplication.materiasdatabase?.MateriaDao()?.loadById( id )
 
-        println(materia?.materia_db)
+        // controle do estado
+        var acertos = 0
+        var resposta = 0
+        var progresso = 0
 
-        fun escolherPerguntas ( nPerguntas: Int ) : MutableList<Array<CharSequence>> {
-            val arrPerguntas = mutableListOf<Array<CharSequence>>()
+        // número de perguntas que vão fazer parte do jogo
+        var nPerguntas = 10
 
-            if (materia != null) {
-                val arrPerguntasTemp = resources.obtainTypedArray(materia.arrayPerguntas_db)
+        // pegando as perguntas
+        val perguntas = mutableListOf<Array<CharSequence>>()
+        if (materia != null) {
+            val arrPerguntasTemp = resources.obtainTypedArray(materia.arrayPerguntas_db)
 
-                var qntPerguntas = arrPerguntasTemp.length()
+            val qntPerguntasTemp = arrPerguntasTemp.length()
 
-                if (qntPerguntas > nPerguntas)
-                    qntPerguntas = nPerguntas
+            if (nPerguntas > qntPerguntasTemp)
+                nPerguntas = qntPerguntasTemp
 
-                val ordemPerguntas = (0 until qntPerguntas).toMutableList()
-                ordemPerguntas.shuffle()
+            val ordemPerguntas = (0 until nPerguntas).toMutableList()
+            ordemPerguntas.shuffle()
 
-                for (i in 0 until qntPerguntas)
-                    arrPerguntas.add( arrPerguntasTemp.getTextArray(ordemPerguntas[i]) )
-
-                arrPerguntasTemp.recycle()
-            }
-            return arrPerguntas
+            for (i in 0 until nPerguntas)
+                perguntas.add(arrPerguntasTemp.getTextArray(ordemPerguntas[i]))
+            arrPerguntasTemp.recycle()
         }
 
-        // declarando os elementos do layout
+        // elementos do layout
+        val fundoRespostaDrawable = ContextCompat.getDrawable(this, R.drawable.card_respostas_fundo)
+        val fundoSelecionadoDrawable = ContextCompat.getDrawable(this,
+            R.drawable.card_respostas_selecionada
+        )
+
+        val somBom = MediaPlayer.create(this, R.raw.certo)
+        val somRuim = MediaPlayer.create(this, R.raw.errado)
+
         val corFundo = findViewById<RelativeLayout>(R.id.cor_fnd)
         val imgFundo = findViewById<RelativeLayout>(R.id.img_fnd)
         val progressoBarra = findViewById<ProgressBar>(R.id.progressoBarra)
@@ -65,81 +75,21 @@ class TelaPerguntas : AppCompatActivity() {
         val txtPergunta = findViewById<TextView>(R.id.pergunta)
         val btnConferir = findViewById<Button>(R.id.btn_conferir)
 
-        val fundoRespostaDrawable = ContextCompat.getDrawable(
-            this,
-            R.drawable.card_respostas_fundo
-        )
-        val fundoSelecionadoDrawable = ContextCompat.getDrawable(
-            this,
-            R.drawable.card_respostas_selecionada
-        )
-
-        val somBom = MediaPlayer.create(this, R.raw.certo)
-        val somRuim = MediaPlayer.create(this, R.raw.errado)
-
-        // desenhando a view
+        // desenha a base da tela
         if (materia != null) {
-            corFundo.setBackgroundColor(getColor(materia.cor_db))
-            imgFundo.setBackgroundResource(materia.fundo_db)
+            corFundo.setBackgroundColor( getColor(materia.cor_db) )
+            imgFundo.setBackgroundResource( materia.fundo_db )
             if (materia.isPreto_db)
                 txtPergunta.setTextColor(getColor(R.color.colorPrt))
             else
                 txtPergunta.setTextColor(getColor(R.color.colorBnc))
         }
 
-        val nPerguntas = 10
-        var acertos = 0
-        var progresso = 0
-        var resposta = 0
-
-        val perguntas = escolherPerguntas(nPerguntas)
-
-        btnConferir.setOnClickListener {
-            if (resposta == 1) {
-                acertos += 1
-                progressoBarra.progressTintList = ColorStateList.valueOf(
-                    Color.rgb(50, 180, 75)
-                )
-                somBom.start()
-            }
-            else {
-                progressoBarra.progressTintList = ColorStateList.valueOf(
-                    Color.rgb(235, 5, 0)
-                )
-                somRuim.start()
-            }
-            progresso += 1
-            progressoBarra.progress =
-                ((progresso.toDouble() / nPerguntas.toDouble()) * 100).toInt()
-            btnConferir.isEnabled = false
-        }
-
-        fun montarResposta( resposta: CharSequence ) : CardResposta {
-            val cardLayout = CardResposta(this)
-            val param = GridLayout.LayoutParams(
-                GridLayout.spec(GridLayout.UNDEFINED, GridLayout.FILL, 1f),
-                GridLayout.spec(GridLayout.UNDEFINED, GridLayout.FILL, 1f)
-            )
-            param.width = 0
-            param.setMargins(resources.getDimension(R.dimen.margem_meia_margin).toInt())
-            cardLayout.layoutParams = param
-
-            // adicionando o texto da resposta
-            cardLayout.setRespostaText(
-                resposta
-            )
-            cardLayout.setRespostaDrawable(fundoRespostaDrawable)
-
-            return cardLayout
-        }
-
-        fun montarPergunta( pergunta: Array<CharSequence>) {
-            txtPergunta.text = perguntas[progresso][0]
-
-            val qntRespostas = pergunta.size - 1
-            val ordemRespostas = (1..qntRespostas).toMutableList()
+        fun getPergunta(indice: Int) {
+            txtPergunta.text = perguntas[indice][0]
+            val qntRespostas = perguntas[indice].size - 1
+            val ordemRespostas = (1 .. qntRespostas).toMutableList()
             ordemRespostas.shuffle()
-
             val respostaCards = mutableListOf<CardResposta>()
 
             // remove os elementos passados do layout
@@ -148,11 +98,23 @@ class TelaPerguntas : AppCompatActivity() {
 
             // começa a desenhar os cards de resposta
             for (i in 0 until qntRespostas) {
-                val cardResposta = montarResposta( pergunta[ordemRespostas[i]] )
-                respostaCards.add( cardResposta )
+                val cardLayout = CardResposta(this)
+                val param = GridLayout.LayoutParams(
+                    GridLayout.spec(GridLayout.UNDEFINED, GridLayout.FILL, 1f),
+                    GridLayout.spec(GridLayout.UNDEFINED, GridLayout.FILL, 1f)
+                )
+                param.width = 0
+                param.setMargins(resources.getDimension(R.dimen.margem_meia_margin).toInt())
+                cardLayout.layoutParams = param
 
-                gridPergunta.addView( cardResposta )
-                cardResposta.setOnClickListener {
+                // adicionando o texto da resposta
+                cardLayout.setRespostaText(
+                    perguntas[indice][ordemRespostas[i]]
+                )
+                cardLayout.setRespostaDrawable(fundoRespostaDrawable)
+                respostaCards.add(cardLayout)
+                gridPergunta.addView(cardLayout)
+                cardLayout.setOnClickListener {
                     resposta = ordemRespostas[i]
                     if (!btnConferir.isEnabled)
                         btnConferir.isEnabled = true
@@ -167,17 +129,42 @@ class TelaPerguntas : AppCompatActivity() {
             }
         }
 
-        while(progresso < nPerguntas) {
-            montarPergunta( perguntas[progresso] )
-        }
-        val pontos = (acertos.toDouble() / nPerguntas.toDouble()) * 100
         if (materia != null) {
-            if (pontos > materia.pontos_db) {
-                materia.pontos_db = pontos.toInt()
-                MyApplication.materiasdatabase?.MateriaDao()?.updateCatg(materia)
+            getPergunta(progresso)
+
+            btnConferir.setOnClickListener {
+                if (resposta == 1) {
+                    acertos += 1
+                    progressoBarra.progressTintList = ColorStateList.valueOf(
+                        Color.rgb(50, 180, 75)
+                    )
+                    somBom.start()
+                } else {
+                    progressoBarra.progressTintList = ColorStateList.valueOf(
+                        Color.rgb(235, 5, 0)
+                    )
+                    somRuim.start()
+                }
+                progresso += 1
+                progressoBarra.progress =
+                    ((progresso.toDouble() / nPerguntas.toDouble()) * 100).toInt()
+
+                if (progresso < nPerguntas) {
+                    getPergunta(progresso)
+                }
+                else {
+                    val pontos = (acertos.toDouble() / nPerguntas.toDouble()) * 100
+                    if (pontos > materia.pontos_db)
+                        materia.pontos_db = pontos.toInt()
+
+                    MyApplication.materiasdatabase?.MateriaDao()?.updateCatg(materia)
+                    loadTelaResultado( id )
+                }
+
+                btnConferir.isEnabled = false
             }
         }
-        loadTelaResultado( id )
+
     }
 }
 
